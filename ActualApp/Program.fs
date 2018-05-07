0c34e05766fec564d21e765b2b95615eb53e8ee6
Maciek <MagicFleas@bitbucket.org>
2018-05-07 21:12:32 +0200

    initial commit

---
+++ ActualApp/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Program
+
+[<EntryPoint>]
+let main argv =
+    let result = getIds ()
+    printfn "%A" result
+    0 
---
+++ DataAccessApp/Program.fs
@@ -0,0 +1,24 @@
+﻿module Program 
+open FSharp.Data.Sql
+
+[<Literal>]
+let CompiletimeConnectionString = "Server=localhost;Port=5432;Database=postgres;User Id=Dev;Password=dev;"
+
+[<Literal>]
+let ResolutionPath = @".\libs"
+
+type Db = 
+    SqlDataProvider<Common.DatabaseProviderTypes.POSTGRESQL,ConnectionString = CompiletimeConnectionString,ResolutionPath = ResolutionPath>
+
+let getIds () =
+    let dataContext = Db.GetDataContext CompiletimeConnectionString
+
+    dataContext.Public.Buildings
+    |> Seq.map (fun b -> b.Id)
+    
+
+[<EntryPoint>]
+let main argv =
+    let result = getIds ()
+    printfn "%A" result
+    0 
